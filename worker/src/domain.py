4feda788515beb11e314b0bbdd8ade3aee74f880
Paulo Freitas <paulogeo5@gmail.com>
2022-05-06 20:09:05 -0300

    feat: add route to domain

--- worker/src/domain.py
+++ worker/src/domain.py
@@ -1,10 +1,18 @@
 import json
+from typing import NewType
 from dataclasses import dataclass
+from shapely.geometry.linestring import LineString
+from src.geo import load_linestring
+
+
+
+Wkt = NewType('Wkt', str)
 
 
 @dataclass(frozen=True)
 class Position:
     """Position model"""
+
     lat: float
     lng: float
     vehicle_id: int
@@ -15,3 +23,17 @@ class Position:
         """creates a position object from a json string"""
         position = json.loads(json_string)
         return Position(**position)
+
+
+@dataclass(frozen=True)
+class Route:
+    """Route model"""
+
+    id: int
+    code: str
+    name: str
+    geom: Wkt
+
+    @property
+    def linestring(self) -> LineString:
+        return load_linestring(self.geom)
